f392fcc0853537cb1b3463f0244dc696ee5f02a8
Brian Warehime <brianwarehime@gmail.com>
2015-02-13 15:57:49 -0500

    update

--- uf_only.sh
+++ uf_only.sh
@@ -2,7 +2,7 @@
 INSTALL_FILE="splunkforwarder-6.1.6-249101-Linux-x86_64.tgz"
 SPLUNK_INDEXER="omgpwnd.no-ip.biz:9997"
 HOST_NAME="hp-md-01"
-KIPPO_LOG_LOCATION="/opt/kippo/log/kippo.log.*"
+KIPPO_LOG_LOCATION='/opt/kippo/log/kippo.log.*'
 
 # Adding required users
 useradd splunk
@@ -40,7 +40,7 @@ cd Tango
 mv tango_input /opt/splunkforwarder/etc/apps/
 cd /opt/splunkforwarder/etc/apps/tango_input/default
 sed -i "s/test/$HOST_NAME/" inputs.conf
-#sed -i "s/\/opt\/kippo\/log\/kippo.log/$KIPPO_LOG_LOCATION/" inputs.conf
+sed -i "s,/opt/kippo/log/kippo.log,${KIPPO_LOG_LOCATION}," inputs.conf
 
 chown -R splunk:splunk /opt/splunkforwarder
 /opt/splunkforwarder/bin/splunk restart
